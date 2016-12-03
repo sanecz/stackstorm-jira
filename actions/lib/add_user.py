237fa2eedadc740c8b3eaeaf45978af245a11639
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 12:03:25 +0100

    Fix add user

--- actions/lib/add_user.py
+++ actions/lib/add_user.py
@@ -3,5 +3,5 @@ from base import JiraBaseAction
 
 class JiraAddUser(JiraBaseAction):
     def _run(self, username, email, **kwargs):
-      return self.jira.add_user(username, email)
+      return self.jira.add_user(username, email, **kwargs)
 
