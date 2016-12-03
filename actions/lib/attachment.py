78849ddc2228d833bece18c9adb47142c868bb0e
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 12:03:34 +0100

    Fix attachment

--- actions/lib/attachment.py
+++ actions/lib/attachment.py
@@ -1,7 +1,10 @@
 from base import JiraBaseAction
-
+import os
 
 class JiraAttachment(JiraBaseAction):
     def _run(self, id):
-      return self.jira.attachment(id)
+      attachment = self.jira.attachment(id)
+      with open(os.path.join(self.attachment_dir, attachment.filename), "w") as handle:
+          handle.write(attachment.get())
+      return attachment.filename
 
