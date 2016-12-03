25bbd774cf96196fe6d76252248dec3bd6281d72
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 22:56:15 +0100

    Fix create project

--- actions/lib/create_project.py
+++ actions/lib/create_project.py
@@ -3,5 +3,5 @@ from base import JiraBaseAction
 
 class JiraCreateProject(JiraBaseAction):
     def _run(self, key, **kwargs):
-      return self.jira.create_project(key)
+      return self.jira.create_project(key, **kwargs)
 
