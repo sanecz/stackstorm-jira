3a800be34bb849f6c2ff9ffdfd502409e7759d83
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 23:21:46 +0100

    Fix create temp user avatar

--- actions/lib/create_temp_user_avatar.py
+++ actions/lib/create_temp_user_avatar.py
@@ -2,6 +2,7 @@ from base import JiraBaseAction
 
 
 class JiraCreateTempUserAvatar(JiraBaseAction):
-    def _run(self, user, filename, size, avatar_img, **kwargs):
-      return self.jira.create_temp_user_avatar(user, filename, size, avatar_img)
+    def _run(self, user, filename, size, **kwargs):
+      avatar_img = open(filename, "r")
+      return self.jira.create_temp_user_avatar(user, filename, size, avatar_img.read(), **kwargs)
 
