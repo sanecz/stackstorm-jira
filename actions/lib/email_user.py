c26b8557dab2d39cf896515da6ff325b946fecc6
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-04 00:01:21 +0100

    Fix email user

--- actions/lib/email_user.py
+++ actions/lib/email_user.py
@@ -3,5 +3,5 @@ from base import JiraBaseAction
 
 class JiraEmailUser(JiraBaseAction):
     def _run(self, user, body, **kwargs):
-      return self.jira.email_user(user, body)
+      return self.jira.email_user(user, body, **kwargs)
 
