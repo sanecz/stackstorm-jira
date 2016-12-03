38fd3e5d18838c85ef558594eef5b5aa983dc118
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 21:50:20 +0100

    Fix create issue

--- actions/lib/create_issue.py
+++ actions/lib/create_issue.py
@@ -2,6 +2,14 @@ from base import JiraBaseAction
 
 
 class JiraCreateIssue(JiraBaseAction):
-    def _run(self, fieldargs, **kwargs):
-      return self.jira.create_issue(fieldargs)
+    def _run(self, key, summary, description, issuetype, fields):
+      all_fields = {
+        "project": {"key": key},
+        "summary": summary,
+        "description": description,
+        "issuetype": {"name": issuetype}
+      }
+      if fields:
+          all_fields.update(fields)
+      return self.jira.create_issue(all_fields)
 
