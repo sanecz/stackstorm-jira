966885cdb8778661925f538f6d3613c63ba8fbfa
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 21:38:17 +0100

    Adding create_board (autogenerated)

---
+++ actions/lib/create_board.py
@@ -0,0 +1,7 @@
+from base import JiraBaseAction
+
+
+class JiraCreateBoard(JiraBaseAction):
+    def _run(self, name, project_ids, **kwargs):
+      return self.jira.create_board(name, project_ids)
+
