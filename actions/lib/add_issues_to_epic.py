e8de70d31f3cd85c73600de6a3a7cb69addfaa48
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 12:03:22 +0100

    Fix add_issues_to_epic

--- actions/lib/add_issues_to_epic.py
+++ actions/lib/add_issues_to_epic.py
@@ -3,5 +3,5 @@ from base import JiraBaseAction
 
 class JiraAddIssuesToEpic(JiraBaseAction):
     def _run(self, epic_id, issue_keys, **kwargs):
-      return self.jira.add_issues_to_epic(epic_id, issue_keys)
+      return self.jira.add_issues_to_epic(epic_id, issue_keys, **kwargs)
 
