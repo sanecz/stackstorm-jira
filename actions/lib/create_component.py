3b7c9bd5b8e54de31530b96a968d26173030a846
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 21:42:00 +0100

    Fix create component

--- actions/lib/create_component.py
+++ actions/lib/create_component.py
@@ -3,5 +3,5 @@ from base import JiraBaseAction
 
 class JiraCreateComponent(JiraBaseAction):
     def _run(self, name, project, **kwargs):
-      return self.jira.create_component(name, project)
+      return self.jira.create_component(name, project, **kwargs)
 
