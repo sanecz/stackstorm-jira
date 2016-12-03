e4af2cedd7bdfe9b9b9118a58eca26d6765b9d4f
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 23:32:50 +0100

    Fix create meta

--- actions/lib/createmeta.py
+++ actions/lib/createmeta.py
@@ -3,5 +3,5 @@ from base import JiraBaseAction
 
 class JiraCreatemeta(JiraBaseAction):
     def _run(self, **kwargs):
-      return self.jira.createmeta()
+      return self.jira.createmeta(**kwargs)
 
