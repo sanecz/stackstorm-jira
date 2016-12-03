09a9dcde51f857d8d5d5ad83e286e535294c45f5
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 12:03:19 +0100

    Fix add_comment

--- actions/lib/add_comment.py
+++ actions/lib/add_comment.py
@@ -2,6 +2,6 @@ from base import JiraBaseAction
 
 
 class JiraAddComment(JiraBaseAction):
-    def _run(self, issue, body, **kwargs):
+    def _run(self, issue, body):
       return self.jira.add_comment(issue, body)
 
