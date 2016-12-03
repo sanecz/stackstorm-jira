01783258420c245aeb3d81e35b9f3438373a9618
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 12:03:16 +0100

    Adding add_attachment (autogenerated)

---
+++ actions/lib/add_attachment.py
@@ -0,0 +1,7 @@
+from base import JiraBaseAction
+
+
+class JiraAddAttachment(JiraBaseAction):
+    def _run(self, issue, attachment, **kwargs):
+      return self.jira.add_attachment(issue, attachment)
+
