fcb6ab22fc0e0b0b52f46ff11690150449afcc85
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 21:46:44 +0100

    Fix create filter

--- actions/lib/create_filter.py
+++ actions/lib/create_filter.py
@@ -3,5 +3,5 @@ from base import JiraBaseAction
 
 class JiraCreateFilter(JiraBaseAction):
     def _run(self, **kwargs):
-      return self.jira.create_filter()
+      return self.jira.create_filter(**kwargs)
 
