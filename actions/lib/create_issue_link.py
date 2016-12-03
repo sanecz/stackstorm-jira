a96f1ea8769433f0a84a89b5a33a9c269183219f
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 22:49:13 +0100

    Fix create issue link

--- actions/lib/create_issue_link.py
+++ actions/lib/create_issue_link.py
@@ -3,5 +3,5 @@ from base import JiraBaseAction
 
 class JiraCreateIssueLink(JiraBaseAction):
     def _run(self, type, inwardIssue, outwardIssue, **kwargs):
-      return self.jira.create_issue_link(type, inwardIssue, outwardIssue)
+      return self.jira.create_issue_link(type, inwardIssue, outwardIssue, **kwargs)
 
