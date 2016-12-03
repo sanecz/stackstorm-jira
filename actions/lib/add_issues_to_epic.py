20c3beca728709137f9c03b67c9e73ab3ee37294
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 12:03:20 +0100

    Adding add_issues_to_epic (autogenerated)

---
+++ actions/lib/add_issues_to_epic.py
@@ -0,0 +1,7 @@
+from base import JiraBaseAction
+
+
+class JiraAddIssuesToEpic(JiraBaseAction):
+    def _run(self, epic_id, issue_keys, **kwargs):
+      return self.jira.add_issues_to_epic(epic_id, issue_keys)
+
