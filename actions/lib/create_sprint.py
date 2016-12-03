b50816cc323080ea2f84750ea99ca0a3b6a75cc0
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 23:02:48 +0100

    Fix create sprint

--- actions/lib/create_sprint.py
+++ actions/lib/create_sprint.py
@@ -3,5 +3,5 @@ from base import JiraBaseAction
 
 class JiraCreateSprint(JiraBaseAction):
     def _run(self, name, board_id, **kwargs):
-      return self.jira.create_sprint(name, board_id)
+      return self.jira.create_sprint(name, board_id, **kwargs)
 
