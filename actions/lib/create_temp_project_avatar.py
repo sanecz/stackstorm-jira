6a983303ea6878dab6046fabbd75949df5113264
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 23:19:30 +0100

    Fix create temp project avatar

--- actions/lib/create_temp_project_avatar.py
+++ actions/lib/create_temp_project_avatar.py
@@ -2,6 +2,7 @@ from base import JiraBaseAction
 
 
 class JiraCreateTempProjectAvatar(JiraBaseAction):
-    def _run(self, project, filename, size, avatar_img, **kwargs):
-      return self.jira.create_temp_project_avatar(project, filename, size, avatar_img)
+    def _run(self, project, filename, size, **kwargs):
+      avatar_img = open(filename, "r")
+      return self.jira.create_temp_project_avatar(project, filename, size, avatar_img.read(), **kwargs)
 
