e1dba11aecd7e9720ae5a06f1fea7cc101f7ee8e
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 12:03:37 +0100

    Adding boards (autogenerated)

--- actions/lib/backup_progress.py
+++ actions/lib/backup_progress.py
@@ -2,6 +2,6 @@ from base import JiraBaseAction
 
 
 class JiraBackupProgress(JiraBaseAction):
-    def _run(self, ):
+    def _run(self):
       return self.jira.backup_progress()
 
---
+++ actions/lib/boards.py
@@ -0,0 +1,7 @@
+from base import JiraBaseAction
+
+
+class JiraBoards(JiraBaseAction):
+    def _run(self, **kwargs):
+      return self.jira.boards()
+
