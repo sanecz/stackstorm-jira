105b1dd93f582140f136e32ff4f95ebb808579a9
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 12:03:24 +0100

    Fix add_remote_link

--- actions/lib/add_remote_link.py
+++ actions/lib/add_remote_link.py
@@ -3,5 +3,5 @@ from base import JiraBaseAction
 
 class JiraAddRemoteLink(JiraBaseAction):
     def _run(self, issue, destination, **kwargs):
-      return self.jira.add_remote_link(issue, destination)
+      return self.jira.add_remote_link(issue, destination, **kwargs)
 
