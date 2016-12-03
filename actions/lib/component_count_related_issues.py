6dd9158a270f0d539c1f25590c572ba99366d64c
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-03 21:33:29 +0100

    Fix component

--- actions/lib/component_count_related_issues.py
+++ actions/lib/component_count_related_issues.py
@@ -3,5 +3,5 @@ from base import JiraBaseAction
 
 class JiraComponentCountRelatedIssues(JiraBaseAction):
     def _run(self, id):
-      return self.jira.component_count_related_issues(id)
+      return self.jira.component_count_related_issues(str(id))
 
