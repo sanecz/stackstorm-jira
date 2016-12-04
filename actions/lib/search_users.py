236a6be6e41b9b118be0adba60d14e9f7111750a
Lisa Bekdache <lisa.bekdache@gmail.com>
2016-12-04 21:55:21 +0100

    Adding security_level (autogenerated)

--- actions/lib/search_users.py
+++ actions/lib/search_users.py
@@ -3,5 +3,5 @@ from base import JiraBaseAction
 
 class JiraSearchUsers(JiraBaseAction):
     def _run(self, user, **kwargs):
-      return self.jira.search_users(user)
+      return self.jira.search_users(user, **kwargs)
 
