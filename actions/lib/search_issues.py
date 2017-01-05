8f84ac91a47841ea4b1fe63db8a34da4fdf75a9d
Lisa Bekdache <lisa.bekdache@dailymotion.com>
2017-01-05 10:59:27 +0100

    Fix search issues

--- actions/lib/search_issues.py
+++ actions/lib/search_issues.py
@@ -3,5 +3,6 @@ from base import JiraBaseAction
 
 class JiraSearchIssues(JiraBaseAction):
     def _run(self, jql_str, **kwargs):
-      return self.jira.search_issues(jql_str, **kwargs)
+      issues = self.jira.search_issues(jql_str, json_result=True, **kwargs)
+      return issues
 
